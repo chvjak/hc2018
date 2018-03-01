ee1ced9c107be719b1db9be1395ebc176333700e
Misha Zhukov <3v1l91l@gmail.com>
2018-03-01 20:23:09 +0200

    initial

---
+++ main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from lib import *
+
+def main():
+    rows_num, col_num, vehicles_num, rides_num, ride_bonus, steps_num, data = load_data('a_example.in')
+
+if __name__ == '__main__':
+    main()
